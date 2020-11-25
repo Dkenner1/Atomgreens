5f1b9b3399edaca25cc2b527f967647c5f675304
Dkenner1 <dkenner@learningframework.com>
2020-11-25 14:57:17 -0800

    Inserted Chart Javascript library into static folder

--- MicrogreensUI/app.py
+++ MicrogreensUI/app.py
@@ -15,12 +15,19 @@ def start_server():
     app.run(host='localhost', port=1111, threaded=True)
 
 
+def start_db():
+    print('~ ' * 5)
+    print('in start_server')
+    app.logger.setLevel(logging.INFO)
+    app.run(host='localhost', port=1111, threaded=True)
+
+
 if __name__ == '__main__':
     t = Thread(target=start_server)
     t.daemon = True
     t.start()
     width = int(1920 * 0.75)
     height = int(1080 * .75)
-    window = webview.create_window('Microgreens', 'http://localhost:1111/', width=width,
+    window = webview.create_window('MicrogreensUI', 'http://localhost:1111/', width=width,
                                    height=height, )
-    webview.start(gui="qt", debug=True)
+    webview.start(debug=True)
