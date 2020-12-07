6eeb70b8c8b44f9a0fa24fcb782be687a8731630
Dkenner1 <dkenner@learningframework.com>
2020-12-06 19:12:03 -0800

    Removed Pycache

--- MicrogreensUI/routes/main.py
+++ MicrogreensUI/routes/main.py
@@ -8,7 +8,7 @@ main = Blueprint('main', __name__, template_folder='templates')
 @main.route('/', methods=['GET', 'POST'])
 def index():
     #mongodb query {item['sensor']: item['value'] for item in status.find({'tray': 1}, {'_id:': 0})}
-    data = {'tray_status': [{'id': 1, 'iscomplete': True}, {'id': 1, 'iscomplete': False}, {'id': 3, 'iscomplete': False},  {'id': 4, 'iscomplete': True}], 'water_level': 11}
+    data = {'tray_status': [{'id': 1, 'iscomplete': True}, {'id': 1, 'iscomplete': False}, {'id': 3, 'iscomplete': False},  {'id': 4, 'iscomplete': True}], 'water_level': 11, 'weight': 1}
     return render_template('index.html', status=data)
 
 
@@ -19,8 +19,9 @@ def settings():
 
 @main.route('/trayinfo/<trayid>', methods=['GET', 'POST'])
 def data(trayid):
-
-
+    #ph_data = tray_data.find({'tray': int(trayid), 'sensor': 'ph'}, {'_id': 0})
+    #weight_data = tray_data.find({'tray': int(trayid), 'sensor': 'ph'}, {'_id': 0})
+    #water_data = tray_data.find({'tray': int(trayid), 'sensor': 'water level'}, {'_id': 0})
     return render_template('tray.html')
 
 # eli was here
