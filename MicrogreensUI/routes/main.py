4bfcf906caaf12caf2a23398c48ae45c223ed4bb
Dkenner1 <dkenner@learningframework.com>
2021-01-02 18:01:24 -0800

    Updated SQL calls file.

--- MicrogreensUI/routes/main.py
+++ MicrogreensUI/routes/main.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, render_template, json, \
     redirect, request, url_for
 from util.db import connect
+from util.SQL import *
 import time
 
 main = Blueprint('main', __name__, template_folder='templates')
@@ -10,8 +11,7 @@ main = Blueprint('main', __name__, template_folder='templates')
 def index():
     conn = connect()
     cur = conn.cursor()
-    sqlstr = """SELECT sensor, val FROM pi4_status"""
-    data = {item[0].replace(' ', '_'): item[1] for item in cur.execute(sqlstr)}
+    data = {item[0].replace(' ', '_'): item[1] for item in cur.execute(PI4_STATUS)}
     conn.close()
     return render_template('index.html', status=data)
 
@@ -23,11 +23,18 @@ def settings():
 
 @main.route('/trayinfo/<trayid>', methods=['GET', 'POST'])
 def data(trayid):
+    conn = connect()
+    cur = conn.cursor()
     etime = time.time()
     time_range = etime-604800 # 1 week period
-    #queryStr = "SELECT * FROM data WHERE time >=" + str(time_range)
-    #result = query(queryStr)
-    return render_template('tray.html')
+    data = {}
+    for tup in cur.execute(SELECT_PI_SENSOR_BETWEEN, (trayid, time_range, etime)).fetchall():
+        if tup[0] in data:
+           data[tup[0]].append((tup[1], tup[2]))
+        else:
+            data[tup[0]] = [(tup[1], tup[2])]
+    conn.close()
+    return render_template('tray.html', data=data)
 
 @main.route('/service', methods=['GET', 'POST'])
 def service():
--- MicrogreensUI/util/SQL.py
+++ MicrogreensUI/util/SQL.py
@@ -9,6 +9,18 @@ SELECT_VIEW_NAMES = "SELECT name FROM sqlite_master WHERE type='view'"
 EN_FK = "PRAGMA foreign_keys = 1"
 DIS_FK = "PRAGMA foreign_keys = 0"
 
+SELECT_PI_SENSOR_ALL = """SELECT sensors.sensor AS sensor, measurements.val AS val, measurements.epoch_time AS etime FROM measurements
+                            INNER JOIN active_devices ON active_devices.id = measurements.devId
+                            INNER JOIN sensors ON sensors.id = active_devices.sensorId
+                            WHERE active_devices.piId = ?; """
+
+SELECT_PI_SENSOR_BETWEEN = """SELECT sensors.sensor AS sensor, measurements.val AS val, measurements.epoch_time AS etime FROM measurements
+                            INNER JOIN active_devices ON active_devices.id = measurements.devId
+                            INNER JOIN sensors ON sensors.id = active_devices.sensorId
+                            WHERE active_devices.piId = ? AND measurements.epoch_time BETWEEN ? and ?; """
+
+PI4_STATUS = """SELECT sensor, val FROM pi4_status"""
+
 def create_status_view(name, ID):
     return "CREATE VIEW " + name + " AS SELECT * FROM status WHERE piId=" + str(ID)
 
