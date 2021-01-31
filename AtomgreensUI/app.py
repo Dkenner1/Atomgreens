31f15b74d2b60de325353869462853c3b2751a1f
Dkenner1 <dkenner@learningframework.com>
2021-01-31 14:48:18 -0800

    Basic service page finished

--- AtomgreensUI/app.py
+++ AtomgreensUI/app.py
@@ -7,7 +7,7 @@ from waitress import serve
 
 app = Flask('Microgreens')
 app.register_blueprint(main)
-
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 1
 
 def start_server():
     print('~ ' * 5)
@@ -25,12 +25,12 @@ def start_db():
 
 
 if __name__ == '__main__':
+    start_server()
     t = Thread(target=start_server)
     t.daemon = True
     t.start()
     width = int(1920 * 0.75)
     height = int(1080 * .75)
-
     window = webview.create_window('MicrogreensUI', 'http://localhost:5000', width=width,
-                                   height=height, )
-    webview.start(debug=True)
+                                   height=height)
+    webview.start(gui='qt', debug=True)
