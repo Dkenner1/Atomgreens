3e529397482a477e5af03b902a6581c311d3647c
EliasWooten <71834885+EliasWooten@users.noreply.github.com>
2021-02-24 18:33:36 -0800

    PI0 Done BB
    FInished writing the pi0 scheduling, boot, and sensor files along with how they interact with the serial file.
    Moved all pi0 files into the PI0 control folder in the GIT

---
+++ pi0_control/Solinoid.py
@@ -0,0 +1,21 @@
+import RPi.GPIO as GPIO
+from time import sleep
+import time
+import serial 
+import utcp
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(31,GPIO.OUT) #sets watering actuator as output
+GPIO.output(31, GPIO.LOW) #turn off the device 
+
+class setPWM:
+    def recive(msg):
+        if (msg == 0):
+            GPIO.output(31, GPIO.LOW) #turn off the device 
+        elif (msg == 1):
+            GPIO.output(31, GPIO.HIGH) #turn on the device 
+        else:
+            #handle error
+            sender.send(0, 4, {"Error": 1}) #PI id, Sensor ID, data
+        return
