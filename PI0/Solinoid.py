ec6bfd0ef42e507aea8a1a12c5b0561e5b32b149
eliaswooten <facepalmsareus@gmail.com>
2021-03-18 10:55:08 -0700

    updated the solinoid file to be callable (it got completely errased not too long ago)

--- PI0/Solinoid.py
+++ PI0/Solinoid.py
@@ -6,10 +6,16 @@ ser = serial.Serial(port="/dev/serial0", baudrate=9600)
 sender = UTCP(ser)
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
+solinoid = 31
+GPIO.setup(solinoid, GPIO.OUT)
+GPIO.output(solinoid, GPIO.LOW)
 
 class solinoid:
-    def Open():
-        
-        return 
+    def actuate(command):
+        if (command == 1): #if we want to turn on the solinoid 
+            GPIO.output(solinoid, GPIO.HIGH)
+        else: 
+            GPIO.output(solinoid, GPIO.LOW)
+    
 
 
