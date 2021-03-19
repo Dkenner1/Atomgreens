0849a22fac4accadc440f1f5f8a6f406a15fd6a6
abigaildabu <44449631+abigaildabu@users.noreply.github.com>
2021-03-19 11:58:37 -0700

    PH & EC sensor file

---
+++ PI4/ph_ec_sensors.py
@@ -0,0 +1,27 @@
+import RPi.GPIO as GPIO
+from time import sleep
+import ADC_callable
+from DFRobot_PH import readPH
+from DFRobot_EC import readEC
+
+GPIO.setup(13, GPIO.OUT)
+GPIO.setup(15, GPIO.OUT)
+# GPIO.setwarnings(False) #
+
+waterTemp = ADC_callable.ADC.call(0)
+
+GPIO.output(13, GPIO.HIGH)
+ph = ADC_callable.ADC.call(1)
+GPIO.output(13, GPIO.LOW)
+
+GPIO.output(15, GPIO.HIGH)
+ec = ADC_callable.ADC.call(2)
+GPIO.output(15, GPIO.LOW)
+
+temperature = (74.4921 / (waterTemp-3.3)) + 70.1467
+PH = ph.readPH(ph, temperature)
+EC = ec.readEC(ec, temperature)
+
+print("Temperature: %.1f ^C EC: %.2f ms/cm PH: %.2f" % (temperature, EC, PH))
+
+sleep(1.0)
