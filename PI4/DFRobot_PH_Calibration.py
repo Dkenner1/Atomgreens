bca7f7abbfc53a280c2164848217b48ef9dd7d33
eliaswooten <facepalmsareus@gmail.com>
2021-03-20 13:30:08 -0700

    updated the Ph calabration file

--- PI4/DFRobot_PH_Calibration.py
+++ PI4/DFRobot_PH_Calibration.py
@@ -4,29 +4,19 @@ import ADC_callable
 import DFRobot_PH
 #import DFRobot_EC
 
-GPIO.setup(13, GPIO.OUT)
-#GPIO.setup(15, GPIO.OUT)
+GPIO.setmode(GPIO.BOARD)
 GPIO.setwarnings(False)
+GPIO.setup(13, GPIO.OUT)
 
+waterTemp = 25
 
-class PH_Calibration:
-	def readPHTest():
-		waterTemp = 25
+GPIO.output(13, GPIO.HIGH)
+sleep(1)
+ph = ADC_callable.ADC.call(2)
+GPIO.output(13, GPIO.LOW)
 
-		GPIO.output(13, GPIO.HIGH)
-		ph = ADC_callable.ADC.call(1)
-		GPIO.output(13, GPIO.LOW)
+temperature = (74.4921 / (waterTemp - 3.3)) + 70.1467
+temperature = 25
+PH = DFRobot_PH.DFRobot_PH.calibration(ph, temperature)
 
-		#GPIO.output(15, GPIO.HIGH)
-		#ec = ADC_callable.ADC.call(2)
-		#GPIO.output(15, GPIO.LOW)
-
-		temperature = (74.4921 / (waterTemp - 3.3)) + 70.1467
-		temperature = 25
-		PH = DFRobot_PH.ph.calibration(ph, temperature)
-		#EC = ec.DFRobot_EC.calibration(ec, temperature)
-
-		print("PH: %.2f" % (PH))
-
-
-sleep(1.0)
+print(ph)
