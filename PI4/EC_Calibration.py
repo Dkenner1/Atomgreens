dd00e1cad67610d03f0796b65da89223e8ce7077
eliaswooten <facepalmsareus@gmail.com>
2021-03-20 14:24:17 -0700

    made adjustments to the ph and Ec sensor file s

--- PI4/EC_Calibration.py
+++ PI4/EC_Calibration.py
@@ -2,21 +2,21 @@ import RPi.GPIO as GPIO
 from time import sleep
 import ADC_callable
 import DFRobot_PH
-#import DFRobot_EC
+import DFRobot_EC
 
 GPIO.setmode(GPIO.BOARD)
 GPIO.setwarnings(False)
-GPIO.setup(13, GPIO.OUT)
+GPIO.setup(15, GPIO.OUT)
 
 waterTemp = 25
 
-GPIO.output(13, GPIO.HIGH)
+GPIO.output(15, GPIO.HIGH)
 sleep(1)
-ph = ADC_callable.ADC.call(2)
-GPIO.output(13, GPIO.LOW)
+EC = ADC_callable.ADC.call(1)
+GPIO.output(15, GPIO.LOW)
 
 temperature = (74.4921 / (waterTemp - 3.3)) + 70.1467
 temperature = 25
-PH = DFRobot_PH.DFRobot_PH.calibration(ph, temperature)
+EC = DFRobot_EC.DFRobot_EC.calibration(EC, temperature)
 
-print(ph)
+print(EC)
