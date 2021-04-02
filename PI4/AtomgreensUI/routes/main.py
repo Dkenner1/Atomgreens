e7c03665d8fb844f02da17b83a287727ea460128
abigaildabu <44449631+abigaildabu@users.noreply.github.com>
2021-04-01 23:17:52 -0700

    Testing Javascript and database calls

--- PI4/AtomgreensUI/routes/main.py
+++ PI4/AtomgreensUI/routes/main.py
@@ -3,18 +3,27 @@ from flask import Blueprint, render_template, json, \
 from util.db import connect
 from util.SQL import *
 import time
+import datetime
 
 main = Blueprint('main', __name__, template_folder='templates')
-
 
 @main.route('/', methods=['GET', 'POST'])
 def index():
     conn = connect()
     cur = conn.cursor()
     data = {item[0].replace(' ', '_'): item[1] for item in cur.execute(PI4_STATUS)}
+    eTime = time.time()
+    day = 86400
+    hour = 3600
+    now = datetime.date.today()
+    startTimes = [int(((now - (datetime.date.fromtimestamp((eTime - 2 * day - 5 * hour)))).days / 7) * 100),
+                  int(((now - (datetime.date.fromtimestamp((eTime - 3 * day - 2 * hour)))).days / 7) * 100),
+                  int(((now - (datetime.date.fromtimestamp((eTime - 1 * day - 0 * hour)))).days / 7) * 100),
+                  int(((now - (datetime.date.fromtimestamp((eTime - 4 * day - 5 * hour)))).days / 7) * 100),
+                  int(((now - (datetime.date.fromtimestamp((eTime - 5 * day - 2 * hour)))).days / 7) * 100)]
     conn.close()
     print("Page data: " + str(data))
-    return render_template('index.html', status=data)
+    return render_template('index.html', status=data, times=startTimes)
 
 
 @main.route('/trayinfo/<trayid>', methods=['GET', 'POST'])
@@ -29,11 +38,26 @@ def data(trayid):
             data[row[0]].append((row[1], row[2]))
         else:
             data[row[0]] = [(row[1], row[2])]
+
+    eTime = time.time()
+    day = 86400
+    hour = 3600
+    now = datetime.date.today()
+    startTimes = [int(((now - (datetime.date.fromtimestamp((eTime - 2 * day - 5 * hour)))).days / 7) * 100),
+                  int(((now - (datetime.date.fromtimestamp((eTime - 3 * day - 2 * hour)))).days / 7) * 100),
+                  int(((now - (datetime.date.fromtimestamp((eTime - 1 * day - 0 * hour)))).days / 7) * 100),
+                  int(((now - (datetime.date.fromtimestamp((eTime - 4 * day - 5 * hour)))).days / 7) * 100),
+                  int(((now - (datetime.date.fromtimestamp((eTime - 5 * day - 2 * hour)))).days / 7) * 100)]
     conn.close()
-    return render_template('tray.html', data=data)
+    return render_template('tray.html', data=data, times=startTimes)
 
 
 @main.route('/trayinfo/<trayid>/trayControl', methods=['GET', 'POST'])
 def control(trayid):
     print("We got here!")
-    return render_template('trayCtrl.html', data=data)
+    conn = connect()
+    cur = conn.cursor()
+    data = {item[0].replace(' ', '_'): item[1] for item in cur.execute(PI4_STATUS)}
+    conn.close()
+    print("Page data: " + str(data))
+    return render_template('trayCtrl.html', status=data)
