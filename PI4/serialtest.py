1b4d5a5e39ca9fc031d4c9d55a61b1e8ad85790d
eliaswooten <facepalmsareus@gmail.com>
2021-04-03 22:08:30 -0700

    getting the PI4 and PI0 gits lined up x

--- PI4/serialtest.py
+++ PI4/serialtest.py
@@ -3,6 +3,7 @@ from utcp import UTCP
 from listener import listen
 from EventHub import eventHub
 from behaviors import *
+from time import sleep
 
 ser = serial.Serial(port="/dev/serial0", baudrate=9600)  # Open port with baud rate
 sender = UTCP(ser)
@@ -11,14 +12,16 @@ listen(ser, ser)
 eventHub.subscribe(store, 'DEFAULT')
 
 #turn on blue lights
-sender.send(1, 5, 20) # pi id, dev id, data
+sender.send(1, 5, 0) # pi id, dev id, data
 sleep(2)
 #turn on red lights
-sender.send(1, 6, 20) # pi id, dev id, data
+sender.send(1, 6, 0) # pi id, dev id, data
 sleep(2)
 #turn on actuator
+'''
 sender.send(1, 4, 1) # pi id, dev id, data
 sleep(2)
 #turn off actuator
-sender.send(1, 4, 0) # pi id, dev id, data
+sender.send(1, 4, 1) # pi id, dev id, data
 sleep(2)
+'''
